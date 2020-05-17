b7c51e23cbc7329e93202257094041d9480a5893
Shivam <shiv@DESKTOP-4BVFQV7.localdomain>
2020-05-17 21:20:03 +0530

    Fix: Indentation

--- Prime_Number_or_Not/Prime.java
+++ Prime_Number_or_Not/Prime.java
@@ -1,4 +1,5 @@
-import java.util.*; public class Prime {
+import java.util.*;
+public class Prime {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
